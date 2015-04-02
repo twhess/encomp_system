5f7374514f2c266fcf1f33270dbef71916f6aed0
twhess <traviswhess@gmail.com>
2015-04-01 20:44:24 -0400

    first_step
    employee class code

---
+++ employee.rb
@@ -0,0 +1,18 @@
+
+class Employee
+	attr_reader :first_name, :last_name
+	def initialize(first_name, last_name, phone, email)
+		@first_name = first_name.capitalize
+		@last_name = last_name.capitalize
+		@phone = phone
+		@email = email.downcase
+
+	end
+	def to_s
+		"Name: #{@first_name} #{@last_name} Phone:#{@phone} Email:#{@email}"
+	end
+end
+
+emp1 = Employee.new("Travis", "hess", "937-417-xxxx", "xxxxxx@gmail.com.")
+
+puts emp1
